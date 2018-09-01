b206848bb1e697aeafb81d50e37b4c35d1be592f
Josue <josue.eduardo206@gmail.com>
2018-09-01 16:51:43 +0100

    Added missing class from previous commit

---
+++ core/src/main/java/io/joshworks/fstore/core/io/StorageException.java
@@ -0,0 +1,17 @@
+package io.joshworks.fstore.core.io;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StorageException(Throwable cause) {
+        super(cause);
+    }
+
+}
